e59a8103c6f1c2624583643aab45db41442c88c8
Chiraag Jain <78903401+ChiraagJain@users.noreply.github.com>
2022-12-28 15:27:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw() {
+    background('#808080');
+}
+function modelLoaded() {
+    console.log("model is loaded");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
